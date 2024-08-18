cf5d87c218d31955475e8fb7c135741d0f01b819
VedangKanade <vedangk20@gmail.com>
2024-08-18 15:53:59 +0530

    Imporvisations in UI done

---
+++ client/src/components/Footer.js
@@ -0,0 +1,11 @@
+import "./Footer.css";
+
+function Footer() {
+  return (
+    <footer class="footer">
+      <p>&copy; 2024 TEAM Insght. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
